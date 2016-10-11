e85cb1f2970b70dd9acbc3d2c08e2b50f2c517f8
陈兢花 <240985492@qq.com>
2016-10-11 15:25:13 +0800

    update webpack dev

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 if (isDev) {
     app.use(webpackDevMiddleware(compiler, {
