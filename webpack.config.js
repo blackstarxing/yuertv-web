6a9c4555458babf98b500f8f2aa789fc8c803d86
张丽 <2090885356@qq.com>
2016-10-14 12:27:46 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
         index: ['./client/css/index.css', './client/js/index.js',hotMiddlewareScript],
         register: ['./client/css/register.css',hotMiddlewareScript],
         center: ['./client/css/center.css', './client/js/center.js',hotMiddlewareScript],
-        center: ['./client/css/center.css', './client/js/center.js',hotMiddlewareScript],
         register: ['./client/css/register.css',hotMiddlewareScript],
     },
     output: {
