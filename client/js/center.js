284e3636ee3c2c49c80d103834f1f471e5249815
张丽 <2090885356@qq.com>
2016-10-18 09:51:35 +0800

    update

--- client/js/center.js
+++ client/js/center.js
@@ -1,1 +1,8 @@
 // if(obj=='女'){img.src="nv"}else{img.src="nan"}
+$(function(){
+	$(".m-bottom a").on("click",function(){
+		$(this).addClass(".switch").Siblings().removeClass(".switch");
+		$(".m-switch2").hide().eq($(this).index()).show();
+	})
+	$(".m-bottom a:eq(0)").trigger("click");
+})
