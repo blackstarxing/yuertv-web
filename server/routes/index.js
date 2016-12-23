4051f026e7f3df78480f323d69dc876d7622ecda
闫佳兴 <414061962@qq.com>
2016-12-23 12:03:44 +0800

    christmas activity & tashan spread

--- server/routes/index.js
+++ server/routes/index.js
@@ -951,7 +951,8 @@ router.get('/activity/ucg', function(req, res, next) {
 });
 
 router.get('/spread/tashan', function(req, res, next) {
-    var from = req.query.from;
+    var type = req.query.type;
+    var index = req.query.index;
     var islogin = false;
     if(req.headers.cookie){
         if(req.headers.cookie.indexOf('yuer_userId')>=0){
@@ -962,7 +963,7 @@ router.get('/spread/tashan', function(req, res, next) {
     };
     Thenjs.parallel([function(cont) {
         request({
-            uri: 'http://172.16.2.62:8777/index?from='+from,
+            uri: 'http://172.16.2.62:8777/taShanLive?type='+type+'&from=tashan',
             headers: {
                 'User-Agent': 'request',
                 'cookie': req.headers.cookie,
@@ -977,8 +978,42 @@ router.get('/spread/tashan', function(req, res, next) {
     }]).then(function(cont, result) {
         res.render('spread/tashan', {
             title: "娱儿TV--领跑移动电竞的直播平台",
-            index: JSON.parse(result[0]).object.recommend[0],
+            index: JSON.parse(result[0]).object[index],
             islogin: islogin,
+        });
+    }).fail(function(cont, error) { 
+        console.log(error);
+        res.render('error', { title: "错误"});
+    });
+});
+
+router.get('/activity/christmas', function(req, res, next) {
+    Thenjs.parallel([function(cont) {
+        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cont(null, body);
+            } else {
+                cont(new Error('error!'));
+            }
+        })
+    }]).then(function(cont, result) {
+        Thenjs.parallel([function(cont) {
+            console.log(JSON.parse(result[0]).access_token);
+            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    cont(null, body);
+                } else {
+                    cont(new Error('error!'));
+                }
+            })
+        }]).then(function(cont, result) {
+            ticket = JSON.parse(result[0]).ticket;
+            res.render('activity/christmas', {
+                ticket: ticket
+            });
+        }).fail(function(cont, error) { 
+            console.log(error);
+            res.render('error', { title: "错误"});
         });
     }).fail(function(cont, error) { 
         console.log(error);
