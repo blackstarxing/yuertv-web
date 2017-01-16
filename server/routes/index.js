4003f8a301fad3d6be025effee6bce658eacd897
闫佳兴 <414061962@qq.com>
2017-01-16 17:42:00 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,42 @@ var router = express.Router();
 var Thenjs = require('thenjs');
 var request = require('request');
 
+// 微信分享ticket
+var ticket = '';
+var ticketline = '';
+
+function getTicket(){
+    Thenjs.parallel([function(cont) {
+        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cont(null, body);
+            } else {
+                cont(new Error('error!'));
+            }
+        })
+    }]).then(function(cont, result) {
+        Thenjs.parallel([function(cont) {
+            console.log(JSON.parse(result[0]).access_token);
+            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    cont(null, body);
+                } else {
+                    cont(new Error('error!'));
+                }
+            })
+        }]).then(function(cont, result) {
+            ticket = JSON.parse(result[0]).ticket;
+            ticketline = new Date().getTime();
+        }).fail(function(cont, error) { 
+            console.log(error);
+            res.render('error', { title: "错误"});
+        });
+    }).fail(function(cont, error) { 
+        console.log(error);
+        res.render('error', { title: "错误"});
+    });
+}
+
 router.get('/', function(req, res, next) {
     var from = req.query.from;
     var islogin = false;
@@ -434,10 +470,19 @@ router.get('/allvideo', function(req, res, next) {
 });
 
 router.get('/liveShare', function(req, res, next) {
-    var id = req.url.split('=')[1];
-    var ticket = '';
+    var id = req.query.id;
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
     Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
+        request({
+            uri: 'http://172.16.2.62:8777/live/detail?id='+id,
+            headers: {
+                'User-Agent': 'request',
+                'cookie': req.headers.cookie,
+            },
+        }, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 cont(null, body);
             } else {
@@ -445,45 +490,12 @@ router.get('/liveShare', function(req, res, next) {
             }
         })
     }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            Thenjs.parallel([function(cont) {
-                request({
-                    uri: 'http://172.16.2.62:8777/live/detail?id='+id,
-                    headers: {
-                        'User-Agent': 'request',
-                        'cookie': req.headers.cookie,
-                    },
-                }, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        cont(null, body);
-                    } else {
-                        cont(new Error('error!'));
-                    }
-                })
-            }]).then(function(cont, result) {
-                res.render('liveShare', {
-                    title: JSON.parse(result[0]).object.info.title,
-                    result: JSON.parse(result[0]).object,
-                    id:id,
-                    link:JSON.parse(result[0]).object.info.rtmp.replace(/rtmp:/, "http:").replace(/rtmp/, "hls")+'.m3u8',
-                    ticket:ticket,
-                });
-            }).fail(function(cont, error) { 
-                console.log(error);
-                res.render('error', { title: "错误"});
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
+        res.render('liveShare', {
+            title: JSON.parse(result[0]).object.info.title,
+            result: JSON.parse(result[0]).object,
+            id:id,
+            link:JSON.parse(result[0]).object.info.rtmp.replace(/rtmp:/, "http:").replace(/rtmp/, "hls")+'.m3u8',
+            ticket:ticket,
         });
     }).fail(function(cont, error) { 
         console.log(error);
@@ -551,38 +563,13 @@ router.get('/activity/cecforum', function(req, res, next) {
     // res.render('cecforum', { title: "CEC" });
 });
 router.get('/activity/propage', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            console.log(JSON.parse(result[0]).ticket);
-            res.render('activity/propage', {
-                ticket: JSON.parse(result[0]).ticket,
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('activity/propage', {
+        ticket: ticket
     });
-    // res.render('activity/propage', { title: "百万主播招募活动页" });
 });
 
 router.get('/mobile/down', function(req, res, next) {
@@ -590,38 +577,13 @@ router.get('/mobile/down', function(req, res, next) {
 });
 
 router.get('/mobile/author', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            console.log(JSON.parse(result[0]).ticket);
-            res.render('mobile/author', {
-                ticket: JSON.parse(result[0]).ticket
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('mobile/author', {
+        ticket: ticket
     });
-    // res.render('mobile/author', { title: "娱儿TV--领跑移动电竞的直播平台" });
 });
 
 router.get('/activity/recruit', function(req, res, next) {
@@ -630,10 +592,18 @@ router.get('/activity/recruit', function(req, res, next) {
 
 router.get('/mobile/activityShare', function(req, res, next) {
     var id = req.query.id;
-    // var id = req.url.split('=')[1];
-    var ticket = '';
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
     Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
+        request({
+            uri: 'http://172.16.2.62:8777/activity/detail?id='+id,
+            headers: {
+                'User-Agent': 'request',
+                'cookie': req.headers.cookie,
+            },
+        }, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 cont(null, body);
             } else {
@@ -641,43 +611,10 @@ router.get('/mobile/activityShare', function(req, res, next) {
             }
         })
     }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            Thenjs.parallel([function(cont) {
-                request({
-                    uri: 'http://172.16.2.62:8777/activity/detail?id='+id,
-                    headers: {
-                        'User-Agent': 'request',
-                        'cookie': req.headers.cookie,
-                    },
-                }, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        cont(null, body);
-                    } else {
-                        cont(new Error('error!'));
-                    }
-                })
-            }]).then(function(cont, result) {
-                res.render('mobile/activityShare', {
-                    title: JSON.parse(result[0]).object.info.title,
-                    result: JSON.parse(result[0]).object,
-                    ticket:ticket,
-                });
-            }).fail(function(cont, error) { 
-                console.log(error);
-                res.render('error', { title: "错误"});
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
+        res.render('mobile/activityShare', {
+            title: JSON.parse(result[0]).object.info.title,
+            result: JSON.parse(result[0]).object,
+            ticket:ticket,
         });
     }).fail(function(cont, error) { 
         console.log(error);
@@ -933,36 +870,12 @@ router.get('/activity/hrecruit', function(req, res, next) {
     res.render('activity/hrecruit', { title: "主播招募" });
 });
 router.get('/activity/general', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            res.render('activity/general', {
-                ticket: ticket
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('activity/general', {
+        ticket: ticket
     });
 });
 router.get('/activity/ucg', function(req, res, next) {
@@ -1091,70 +1004,22 @@ router.get('/activity/christmas', function(req, res, next) {
 });
 
 router.get('/activity/anchorRecruit', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            res.render('activity/anchorRecruit', {
-                ticket: ticket
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('activity/anchorRecruit', {
+        ticket: ticket
     });
 });
 
 router.get('/activity/conduct', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            res.render('activity/conduct', {
-                ticket: ticket
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('activity/conduct', {
+        ticket: ticket
     });
 });
 
@@ -1167,36 +1032,12 @@ router.get('/mobile/liveagreement', function(req, res, next) {
 });
 
 router.get('/activity/pvp', function(req, res, next) {
-    Thenjs.parallel([function(cont) {
-        request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf96f728533f32fa8&secret=5007eda46723c5faf79a8b9ca3be131a', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                cont(null, body);
-            } else {
-                cont(new Error('error!'));
-            }
-        })
-    }]).then(function(cont, result) {
-        Thenjs.parallel([function(cont) {
-            console.log(JSON.parse(result[0]).access_token);
-            request('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='+JSON.parse(result[0]).access_token+'&type=jsapi', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cont(null, body);
-                } else {
-                    cont(new Error('error!'));
-                }
-            })
-        }]).then(function(cont, result) {
-            ticket = JSON.parse(result[0]).ticket;
-            res.render('activity/pvp', {
-                ticket: ticket
-            });
-        }).fail(function(cont, error) { 
-            console.log(error);
-            res.render('error', { title: "错误"});
-        });
-    }).fail(function(cont, error) { 
-        console.log(error);
-        res.render('error', { title: "错误"});
+    var nowtime = new Date().getTime();
+    if(!ticket || (nowtime-ticketline)>7000){
+        getTicket();
+    }
+    res.render('activity/pvp', {
+        ticket: ticket
     });
 });
 
