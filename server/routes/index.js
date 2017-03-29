999f8d8f6fbd96834fd15dd562d2fb8336a7616d
闫佳兴 <yanjx@miqtech.com>
2017-03-29 17:35:14 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,10 @@ var request = require('request');
 var ticket = '';
 var ticketline = '';
 
-var path = 'http://172.16.10.6:8777';
-var apipath ="http://172.16.10.134:8099";
-// var path = 'http://webapi.yuerlive.cn';
-// var apipath ="http://api.yuerlive.cn";
+// var path = 'http://172.16.10.6:8777';
+// var apipath ="http://172.16.10.134:8099";
+var path = 'http://webapi.yuerlive.cn';
+var apipath ="http://api.yuerlive.cn";
 
 function getTicket(){
     Thenjs.parallel([function(cont) {
@@ -1435,6 +1435,7 @@ router.get('/activity/wolf', function(req, res, next) {
         res.render('activity/wolf', {
             one: JSON.parse(result[0]).object.info,
             result: JSON.parse(result[0]).object,
+            link:JSON.parse(result[0]).object.info.rtmp.replace(/rtmp:/, "http:").replace(/rtmp/, "hls")+'.m3u8',
             ticket:ticket
         });
     }).fail(function(cont, error) { 
