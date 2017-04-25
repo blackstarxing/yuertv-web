c70bb9bb2b94d806c3a8e70c4b48a2de00118d0d
zhaoxiaofan <18301545298@163.com>
2017-04-25 09:30:14 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,10 @@ var request = require('request');
 var ticket = '';
 var ticketline = '';
 
-// var path = 'http://172.16.10.3:8777';
-// var apipath ="http://172.16.10.134:8099";
-var path = 'http://qa.webapi.yuerlive.cn';
-var apipath ="http://qa.api.yuerlive.cn";
+var path = 'http://172.16.10.3:8777';
+var apipath ="http://172.16.10.3:8099";
+// var path = 'http://qa.webapi.yuerlive.cn';
+// var apipath ="http://qa.api.yuerlive.cn";
 
 function getTicket(){
     Thenjs.parallel([function(cont) {
