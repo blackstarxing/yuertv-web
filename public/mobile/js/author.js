257bead57798abb4690aec80fc870be5e703b113
闫佳兴 <yanjx@miqtech.com>
2017-05-18 17:49:15 +0800

    update

--- public/mobile/js/author.js
+++ public/mobile/js/author.js
@@ -455,11 +455,6 @@ $(function(){
     	$('.step-two').hide();
     	$('.step-three').show();
     })
-    $('.u-stepimg').click(function(){
-    	jumpsecond = -1;
-    	$('.step-two').hide();
-    	$('.step-three').show();
-    })
     // 发送手播下载地址
     $('.u-download-btn').click(function(){
     	var parm = {};
