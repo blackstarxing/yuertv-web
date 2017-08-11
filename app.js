de2dc950775a2b4be4fff838aba683515c344178
zhaoxiaofan <18301545298@163.com>
2017-08-11 16:25:31 +0800

    update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var proxy = require('express-http-proxy');
 
 var app = express();
 
-var proxyaddress = "http://172.16.10.3:8777";
+var proxyaddress = "http://172.16.10.134:8080";
 
 app.use('/api', proxy('http://172.16.10.3:8777', {
   forwardPath: function(req, res) {
